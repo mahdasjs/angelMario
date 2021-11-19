bc108b7fb79763bffe1d069b5bdd160079efb6bb
mahdasjs <45814320+mahdasjs@users.noreply.github.com>
2021-11-19 16:41:02 +0330

    add exit door

---
+++ skyAngel/Assets/dooe.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dooe : MonoBehaviour
+{
+    public bool flag=false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.E) &&flag==true)
+        {
+            Debug.Log("enter");
+
+        }
+
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            flag = true;
+
+        }
+    }
+}
